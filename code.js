83ef161ce728782e1c7d9e404e8678b56291202a
Simon Kofel <sikofel@gmail.com>
2020-09-23 18:29:39 +0200

    API Call and Callback function added

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 // Here we go
 
-// Call API
+// Init variables
+let city = document.querySelector('#searchField');
+const btnSubmit = document.querySelector('form button');
+
+// Add listeners
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  getRequest();
+
+});
+
+
+function getRequest() {
+  // Call API
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=947cf804c478eb1c791115fe97a3238e`).then(response => {
+    response.json().then(data => {
+      console.log(data);
+    });
+  });  
+}
