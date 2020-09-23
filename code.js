afcfae806b75788c37629ee990204fe5ebc33260
Simon Kofel <sikofel@gmail.com>
2020-09-23 18:51:57 +0200

    get icon method

--- code.js
+++ code.js
@@ -3,6 +3,7 @@
 // Init variables
 let city = document.querySelector('#searchField');
 const btnSubmit = document.querySelector('form button');
+const icon = document.querySelector('.icon img');
 
 // Add listeners
 btnSubmit.addEventListener('click', (e) => {
@@ -15,9 +16,15 @@ btnSubmit.addEventListener('click', (e) => {
 
 function getRequest() {
   // Call API
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=947cf804c478eb1c791115fe97a3238e`).then(response => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=947cf804c478eb1c791115fe97a3238e&units=metric`).then(response => {
     response.json().then(data => {
-      console.log(data);
+      console.log(data.weather[0].icon);
+      getIcon(data.weather[0].icon);
     });
   });  
+}
+
+// Get icon
+function getIcon(icon) {
+  icon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 }
